e3661aa4b943c6814cad8dd52c9bc52728389bb6
semrayerlikaya <semrabilgic@gmail.com>
2021-05-30 16:58:54 -0400

    resusable methoddaki login ve logout u kullanma

--- src/test/java/com/hypnotes/tests/US00_DenemeTest.java
+++ src/test/java/com/hypnotes/tests/US00_DenemeTest.java
@@ -7,8 +7,9 @@ import com.hypnotes.utilities.ReusableMethods;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class US00_DenemeTest {
+public class US00_DenemeTest extends ReusableMethods {
     US00_DenemePage US00DenemePage =new US00_DenemePage();
+
     @Test
 public void loginTest() throws InterruptedException {
 
@@ -21,14 +22,16 @@ public void loginTest() throws InterruptedException {
         US00DenemePage.loginButton1.click();
 
 
-
-
         Assert.assertTrue(US00DenemePage.afterLogin.isDisplayed());
         System.out.println("Login test passed");
         Thread.sleep(3000);
         ReusableMethods.clickWithJS(US00DenemePage.logoutButton);
         Thread.sleep(3000);
 
+        // methodla login yapma
+        login(ConfigurationReader.getProperty("semra-email"),ConfigurationReader.getProperty("semra-password"));
+        //methodla logout yapma
+        logout();
 
     }
 }
