a896cb8cb3b8c4d3b185bff79f3965bfbeb589a4
ASUS <ybalkas@gmail.com>
2021-07-22 22:10:39 +0100

    US013_AccountManagement & US013_AccountManagementTest added.

---
+++ src/test/java/com/hypnotes/pages/US009_ClientManagementEdit.java
@@ -0,0 +1,86 @@
+package com.hypnotes.pages;
+
+import com.hypnotes.utilities.Driver;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class US009_ClientManagementEdit {
+    public US009_ClientManagementEdit(){ PageFactory.initElements(Driver.getDriver(),this); }
+    @FindBy(xpath = "//*[.='Contact us']")
+    public List<WebElement> inputBox=new ArrayList<>();
+    @FindBy(xpath="//a[contains(text(),'Clients')]")
+    public WebElement clientsButton;
+    @FindBy(xpath="//*[contains(text(),'View Detail')]")
+    public WebElement viewDetail;
+    @FindBy(xpath="//button[contains(text(),'Edit')]")
+    public WebElement editButton;
+    @FindBy(xpath="//input[@class='form-control infoInput']")
+    public List<WebElement> inputBoxes=new ArrayList<>();
+    @FindBy(xpath="//*[contains(text(),'Save')]")
+    public WebElement saveButton;
+    @FindBy(xpath="//*[contains(text(),'Save')]")
+    public WebElement cancelButton;
+    @FindBy(xpath="//*[@id='inputGroupFile01']")
+    public WebElement browseFile;
+    @FindBy(xpath="//*[contains(text(),'Client Info')]")
+    public WebElement clientInfo;
+    @FindBy(xpath="//*[contains(text(),'Presenting Issue')]")
+    public WebElement presentingIssue;
+    @FindBy(xpath="//*[contains(text(),'Sessions')]")
+    public WebElement sessions;
+    @FindBy (xpath="//div[@class='toast-message']")
+    public WebElement messageUpdated;
+
+   public void navigateToClientTabs() throws InterruptedException {
+     clientsButton.click();
+     Thread.sleep(2000);
+     viewDetail.click();
+     clientInfo.click();
+   }
+    public void inputValidValues(String name, String surname ,String telephone,String Occupation)  {
+        inputBoxes.get(0).clear();
+        inputBoxes.get(0).sendKeys(name);
+        inputBoxes.get(1).clear();
+        inputBoxes.get(1).sendKeys(surname);
+        inputBoxes.get(2).clear();
+        inputBoxes.get(2).sendKeys(telephone);
+        inputBoxes.get(3).clear();
+        inputBoxes.get(3).sendKeys(Occupation);
+        browseFile.sendKeys("C:\\Users\\ASUS\\IdeaProjects\\HyponotesFullTestYB\\src\\test\\resources\\IMG_2830.JPG");
+        saveButton.click();
+    }
+    public void invalidValues(String name, String surname ,String telephone,String Occupation)  {
+        inputBoxes.get(0).clear();
+        inputBoxes.get(0).sendKeys(name);
+        inputBoxes.get(1).clear();
+        inputBoxes.get(1).sendKeys(surname);
+        inputBoxes.get(2).clear();
+        inputBoxes.get(2).sendKeys(telephone);
+        inputBoxes.get(3).clear();
+        inputBoxes.get(3).sendKeys(Occupation);
+        saveButton.click();
+    }
+    public Boolean cancelButton()  {
+       List<String> listBeforeCancel= new ArrayList<>();
+        listBeforeCancel.add(inputBoxes.get(0).getAttribute("value"));
+        listBeforeCancel.add(inputBoxes.get(1).getAttribute("value"));
+        listBeforeCancel.add(inputBoxes.get(2).getAttribute("value"));
+        listBeforeCancel.add(inputBoxes.get(3).getAttribute("value"));
+        System.out.println("listBeforeCancel = " + listBeforeCancel);
+        cancelButton.click();
+        List<String> listafterCancel= new ArrayList<>();
+        listafterCancel.add(inputBoxes.get(0).getAttribute("value"));
+        listafterCancel.add(inputBoxes.get(1).getAttribute("value"));
+        listafterCancel.add(inputBoxes.get(2).getAttribute("value"));
+        listafterCancel.add(inputBoxes.get(3).getAttribute("value"));
+        System.out.println("afterBeforeCancel = " + listafterCancel);
+
+        return listBeforeCancel.equals(listafterCancel);
+
+    }
+}
---
+++ src/test/java/com/hypnotes/tests/US009_ClientManagementEditTest.java
@@ -0,0 +1,94 @@
+package com.hypnotes.tests;
+import com.hypnotes.pages.US009_ClientManagementEdit;
+import com.hypnotes.pages.US013_AccountManagement;
+import com.hypnotes.utilities.ConfigurationReader;
+import com.hypnotes.utilities.Driver;
+import com.hypnotes.utilities.ReusableMethods;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
+
+public class US009_ClientManagementEditTest {
+
+    US009_ClientManagementEdit clientManagement = new US009_ClientManagementEdit();
+    US013_AccountManagement us_013accountManagement= new US013_AccountManagement();
+     WebDriver driver= Driver.getDriver();
+    JavascriptExecutor js = (JavascriptExecutor) driver;
+     Alert alert ;
+    @Test
+    public void TestClientManagement() throws InterruptedException {
+        driver.get(ConfigurationReader.getProperty("hypnotes_link"));
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        us_013accountManagement.loginByLinkedinMethod();
+        us_013accountManagement.viewButton.click();
+        ReusableMethods.clickWithJS(us_013accountManagement.agreementButton1);
+        us_013accountManagement.nextButton.click();
+        js.executeScript("window.scrollBy(0,1000)");
+        ReusableMethods.clickWithJS(us_013accountManagement.agreementButton2);
+        us_013accountManagement.submitButton.click();
+        clientManagement.navigateToClientTabs();
+        js.executeScript("window.scrollBy(0,1000)");
+        Thread.sleep(2000);
+        clientManagement.editButton.click();
+        ReusableMethods.waitFor(3);
+        clientManagement.inputValidValues("Rayaan", "Hayat", "45654654654", "Student");
+        Assert.assertEquals(clientManagement.messageUpdated.getText(), "Client Info has been successfully updated!✅");
+        js.executeScript("window.scrollBy(0,0)");
+        // us_013accountManagement.logoutMethod();
+    }
+
+
+
+    @Test
+    public void editName_withInValid_Credentials() throws InterruptedException {
+        driver.get(ConfigurationReader.getProperty("hypnotes_link"));
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        us_013accountManagement.loginByLinkedinMethod();
+        clientManagement.navigateToClientTabs();
+        js.executeScript("window.scrollBy(0,1000)");
+        Thread.sleep(2000);
+        clientManagement.editButton.click();
+        clientManagement.invalidValues("hi434", "sdh", "435345", "teacher");
+        Assert.assertEquals(clientManagement.messageUpdated.getText(), "Name should not include any digit or special characters");
+      //  loginPage.logoutMethod();
+
+    }
+    @Test
+    public void editSurname_withInValid_CredentialsTest() throws InterruptedException {
+     // loginPage.loginMethod();
+        clientManagement.navigateToClientTabs();
+        clientManagement.invalidValues("hi", "sdh*6", "435345", "teacher");
+        Assert.assertEquals(clientManagement.messageUpdated.getText(), "Surname should not include any digit or special characters");
+      //  loginPage.logoutMethod();
+    }
+    @Test
+    public void editTelephone_withInValid_CredentialsTest() throws InterruptedException {
+     // loginPage.loginMethod();
+        clientManagement.navigateToClientTabs();
+        clientManagement.invalidValues("Esref", "Balks", "435345*7", "teacher");
+        Assert.assertEquals(clientManagement.messageUpdated.getText(), "Please enter valid phone number.");
+      //  loginPage.logoutMethod();
+    }
+    @Test
+    public void requriedFieldsMustnot_beEmptyTest() throws InterruptedException {
+      // loginPage.loginMethod();
+        clientManagement.navigateToClientTabs();
+        clientManagement.invalidValues(" ", " ", " ", "teacher");
+        Thread.sleep(3000);
+        Assert.assertEquals(clientManagement.messageUpdated.getText(), "Required Fields must be filled");
+    //    loginPage.logoutMethod();
+
+    }
+    @Test
+    public void cancelButtonTest() throws InterruptedException {
+     //   loginPage.loginMethod();
+        clientManagement.navigateToClientTabs();
+        Assert.assertTrue(clientManagement.cancelButton());
+       // loginPage.logoutMethod();
+    }
+}
