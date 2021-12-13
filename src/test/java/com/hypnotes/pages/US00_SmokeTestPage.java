f2665a86c5dae3a34b47c44f77a6f9a2b02e9016
Zehra <zehrakoyun06@gmail.com>
2021-12-13 08:39:17 -0500

    Web Elements was added for testing purposes.

--- src/test/java/com/hypnotes/pages/US00_SmokeTestPage.java
+++ src/test/java/com/hypnotes/pages/US00_SmokeTestPage.java
@@ -3,6 +3,7 @@ package com.hypnotes.pages;
 import com.hypnotes.utilities.Driver;
 import com.hypnotes.utilities.ReusableMethods;
 import org.openqa.selenium.WebElement;
+
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
@@ -13,7 +14,8 @@ public class US00_SmokeTestPage {
  public US00_SmokeTestPage() {
   PageFactory.initElements(Driver.getDriver(), this);
  }
-////Login
+
+ ////Login
  @FindBy(xpath = "//a[@href='/api/login']")
  public WebElement loginButton;
  @FindBy(xpath = "//input[@name='email']")
@@ -23,7 +25,7 @@ public class US00_SmokeTestPage {
  @FindBy(xpath = "//button[@type='submit']")
  public WebElement loginButton1;
 
-////////
+ ////////
  @FindBy(xpath = " //h3[contains(text(),'Search Clients')]")
  public WebElement afterLogin;
  @FindBy(xpath = " //a[contains(text(),'Wrong')]")
@@ -31,7 +33,7 @@ public class US00_SmokeTestPage {
  @FindBy(xpath = "//a[@href='/api/logout']")
  public WebElement logoutButton;
 
-//Add/Delete New Client
+ //Add/Delete New Client
  @FindBy(xpath = "//a[@href='/dashboard/add-client']")
  public WebElement addClientButton;
  @FindBy(id = "first_name")
@@ -60,76 +62,76 @@ public class US00_SmokeTestPage {
  public WebElement addNewCategoryButton;
 
 
-//Add/Delete new category
+ //Add/Delete new category
  //@FindBy(xpath = "//input[@placeholder='Please enter a name']")
-@FindBy(id ="title")
+ @FindBy(id = "title")
  public WebElement addNewCategoryNameBox;
- @FindBy(id= "price")
+ @FindBy(id = "price")
  public WebElement addNewCategoryPriceBox;
- @FindBy(id= "duration")
- public WebElement  addNewCategoryDurationDropDown;
- @FindBy(xpath= " //div[@class='rc-virtual-list-holder-inner']/div[1]/div")
+ @FindBy(id = "duration")
+ public WebElement addNewCategoryDurationDropDown;
+ @FindBy(xpath = " //div[@class='rc-virtual-list-holder-inner']/div[1]/div")
  public WebElement duration30Minutes;
- @FindBy(id= "blockBefore")
- public WebElement  blockBeforeBox;
- @FindBy(id= "blockAfter")
+ @FindBy(id = "blockBefore")
+ public WebElement blockBeforeBox;
+ @FindBy(id = "blockAfter")
  public WebElement blockAfterBox;
- @FindBy(id= "paymentRequired")
+ @FindBy(id = "paymentRequired")
  public WebElement paymentRequiredButton;
- @FindBy(id= "description")
+ @FindBy(id = "description")
  public WebElement descriptionBox;
- @FindBy(xpath= "//button[@type='submit']")
+ @FindBy(xpath = "//button[@type='submit']")
  public WebElement saveButton;
- @FindBy(xpath= "//div[contains(text(),'NewCategory1')]/parent::div/parent::div/parent::div//parent::div/ul/li[2]/span/span/*")
+ @FindBy(xpath = "//div[contains(text(),'NewCategory1')]/parent::div/parent::div/parent::div//parent::div/ul/li[2]/span/span/*")
  public WebElement categoryDeleteButton;
- @FindBy(xpath= "//span[contains(text(),'OK')]")
+ @FindBy(xpath = "//span[contains(text(),'OK')]")
  public WebElement categoryDeleteOKButton;
 
  //Add/Delete New Package
- @FindBy(id= "rc-tabs-0-tab-2")
+ @FindBy(id = "rc-tabs-0-tab-2")
  public WebElement packageTab;
- @FindBy(xpath= "//span[contains(text(),'Add New Package')]")
+ @FindBy(xpath = "//span[contains(text(),'Add New Package')]")
  public WebElement addNewpackageButton;
- @FindBy(xpath= "(//div/div/div/input[@placeholder='Please enter a name'])[2]")
+ @FindBy(xpath = "(//div/div/div/input[@placeholder='Please enter a name'])[2]")
  public WebElement packageNameBox;
- @FindBy(xpath= "(//div/input [@id='price'])[2]")
+ @FindBy(xpath = "(//div/input [@id='price'])[2]")
  public WebElement packagePriceBox;
- @FindBy(xpath= "(//input[@id='duration'])[2]")
+ @FindBy(xpath = "(//input[@id='duration'])[2]")
  public WebElement packageDurationBox;
- @FindBy(xpath= " (//div[@class='rc-virtual-list-holder-inner']/div[1]/div)[2]")
+ @FindBy(xpath = " (//div[@class='rc-virtual-list-holder-inner']/div[1]/div)[2]")
  public WebElement packageDuration30Minutes;
- @FindBy(xpath= "(//button/div[@class='ant-switch-handle'])[2]")
+ @FindBy(xpath = "(//button/div[@class='ant-switch-handle'])[2]")
  public WebElement blockExtraTimeButton;
- @FindBy(xpath= "(//input[@id='blockBefore'])[2]")
+ @FindBy(xpath = "(//input[@id='blockBefore'])[2]")
  public WebElement packageBlockBeforeBox;
-//(//input[@id='blockAfter']/parent::div/parent::div/parent::div/parent::div/parent::div/parent::div/*)[4]
- @FindBy(xpath= "(//input[@id='blockAfter'])[2]")
+ //(//input[@id='blockAfter']/parent::div/parent::div/parent::div/parent::div/parent::div/parent::div/*)[4]
+ @FindBy(xpath = "(//input[@id='blockAfter'])[2]")
  public WebElement packageBlockAfterBox;
- @FindBy(xpath= "(//button[@id='paymentRequired'])[2]")
+ @FindBy(xpath = "(//button[@id='paymentRequired'])[2]")
  public WebElement packagePaymentRequiredbutton;
- @FindBy(id= "totalSessions")
+ @FindBy(id = "totalSessions")
  public WebElement totalSessionsBox;
- @FindBy(id= "sessionInterval")
+ @FindBy(id = "sessionInterval")
  public WebElement sessionIntervalBox;
- @FindBy(xpath= "(//textarea[@id='description'])[2]")
+ @FindBy(xpath = "(//textarea[@id='description'])[2]")
  public WebElement packageDescriptionBox;
- @FindBy(xpath= " (//button[@type='submit'])[2]")
+ @FindBy(xpath = " (//button[@type='submit'])[2]")
  public WebElement packageSaveButton;
- @FindBy(xpath= "  //div[contains(text(),'NewPackage1')]/parent::div/parent::div/parent::div/parent::div/ul/li[2]")
+ @FindBy(xpath = "  //div[contains(text(),'NewPackage1')]/parent::div/parent::div/parent::div/parent::div/ul/li[2]")
  public WebElement newPackageDeleteButton;
- @FindBy(xpath= " //span[contains(text(),'OK')]")
+ @FindBy(xpath = " //span[contains(text(),'OK')]")
  public WebElement packageDeleteOKButton;
 
  //Appointment Package1
- @FindBy(xpath= "//div[contains(text(),'Package1')]")
+ @FindBy(xpath = "//div[contains(text(),'Package1')]")
  public WebElement package1Button;
- @FindBy(xpath= "//span[@class='ant-select-selection-item']")
+ @FindBy(xpath = "//span[@class='ant-select-selection-item']")
  public WebElement yearButton;
 
- @FindBy(xpath= "//div[@class='rc-virtual-list-holder-inner']")
+ @FindBy(xpath = "//div[@class='rc-virtual-list-holder-inner']")
  public List<WebElement> dropDownYear;
 
- @FindBy(xpath= "//div[@class='rc-virtual-list-scrollbar-thumb rc-virtual-list-scrollbar-thumb-moving']")
+ @FindBy(xpath = "//div[@class='rc-virtual-list-scrollbar-thumb rc-virtual-list-scrollbar-thumb-moving']")
  public WebElement scrollButton;
 
  @FindBy(xpath = " //div[@label='2021']")
@@ -144,57 +146,97 @@ public class US00_SmokeTestPage {
  @FindBy(xpath = "  //div[@label='Sep']")
  public WebElement monthSep;
 
- @FindBy(xpath= "(//span[@class='ant-select-selection-item'])[2]")
+ @FindBy(xpath = "(//span[@class='ant-select-selection-item'])[2]")
  public WebElement monthButton;
 
 
-
- @FindBy(xpath= "(//div[contains(text(),'23')])[2]")
+ @FindBy(xpath = "(//div[contains(text(),'23')])[2]")
  public WebElement day30;
 
- @FindBy(xpath= "(//div[@class='ant-picker-calendar-date-content'])[24]")
+ @FindBy(xpath = "(//div[@class='ant-picker-calendar-date-content'])[24]")
  public WebElement day23;
 
 
- @FindBy(xpath= "//span[contains(text(),'06:55 AM')]")
+ @FindBy(xpath = "//span[contains(text(),'06:55 AM')]")
  public WebElement time0655Button;
 
- @FindBy(xpath= "//span[contains(text(),'Yes')]")
+ @FindBy(xpath = "//span[contains(text(),'Yes')]")
  public WebElement appointmentYesButton;
- @FindBy(id= "basic_firstName")
+ @FindBy(id = "basic_firstName")
  public WebElement appointmentNameBox;
- @FindBy(id= "basic_lastName")
+ @FindBy(id = "basic_lastName")
  public WebElement appointmentLastNameBox;
- @FindBy(id= "basic_email")
+ @FindBy(id = "basic_email")
  public WebElement appointmentEmailBox;
- @FindBy(id= "basic_phone")
+ @FindBy(id = "basic_phone")
  public WebElement appointmentPhoneBox;
- @FindBy(xpath= "//div[@class='ant-select-selection-overflow']")
+ @FindBy(xpath = "//div[@class='ant-select-selection-overflow']")
  public WebElement majorConcernsDropDown;
- @FindBy(xpath= "//div[@title='Weight Loss Program']")
+ @FindBy(xpath = "//div[@title='Weight Loss Program']")
  public WebElement weightLossButton;
- @FindBy(xpath= "//span[contains(text(),'Submit')]")
+ @FindBy(xpath = "//span[contains(text(),'Submit')]")
  public WebElement appointmentSubmitButton;
- @FindBy(xpath= "//span[contains(text(),'Next')]")
+ @FindBy(xpath = "//span[contains(text(),'Next')]")
  public WebElement appointmentNextButton;
- @FindBy(xpath= " //button[@aria-label='Close']")
+ @FindBy(xpath = " //button[@aria-label='Close']")
  public WebElement closeButton;
 
  //Check For Appoinment
 
- @FindBy(xpath= "//span[contains(text(),'Calendar')]")
+ @FindBy(xpath = "//span[contains(text(),'Calendar')]")
  public WebElement calendarButton;
- @FindBy(xpath= " //a[contains(text(),'Meetings')]")
+ @FindBy(xpath = " //a[contains(text(),'Meetings')]")
  public WebElement meetingsButton;
- @FindBy(xpath= "//div[@class='ant-collapse-header']")
+ @FindBy(xpath = "//div[@class='ant-collapse-header']")
  public WebElement meetInfoButton;
-//(//span[contains(text(),'08:05 - Semra')])[1]
- @FindBy(xpath= "//span[contains(text(),'Cancel Appointment')]")
+ //(//span[contains(text(),'08:05 - Semra')])[1]
+ @FindBy(xpath = "//span[contains(text(),'Cancel Appointment')]")
  public WebElement cancelAppointmentButton;
- @FindBy(xpath= "//span[contains(text(),'Yes')]")
+ @FindBy(xpath = "//span[contains(text(),'Yes')]")
  public WebElement cancelAppointmentYesButton;
 
- @FindBy(xpath= "//span[@class='anticon anticon-close']")
+ @FindBy(xpath = "//span[@class='anticon anticon-close']")
  public WebElement closeMeetInfoButton;
+
+ // Zehra Smoke Test Sign Up as A Client and Sign Up as Therapist
+
+ @FindBy(xpath = "//button[normalize-space()='Sign Up']")
+ public WebElement signUpButton;
+
+ @FindBy(xpath = "//a[normalize-space()='Client']")
+ public WebElement registerClient;
+
+ @FindBy(xpath = "//input[@id='name-input']")
+ public WebElement inputName;
+
+ @FindBy(xpath = "//input[@id='surname-input']")
+ public WebElement inputSurName;
+
+ @FindBy(xpath = "//input[@id='client_registration_form_email']") //input[@id='registration_form_email']
+ public WebElement inputEmail;
+
+ @FindBy(xpath = "//input[@id='pass-input']")
+ public WebElement inputPassword;
+
+ @FindBy(xpath = "//button[@id='register-submit']")
+ public WebElement clickSignUp;
+
+ @FindBy(xpath = "//a[normalize-space()='Therapist']")
+ public WebElement registerTherapist;
+
+ @FindBy(xpath = "//input[@id='fullname-input']")
+ public WebElement inputFullName;
+
+ @FindBy(xpath = "//input[@id='registration_form_email']")
+ public WebElement inputTherapistEmail;
+
+ @FindBy(xpath = "//input[@id='pass-input']")
+ public WebElement inputTherapistPassword;
+
+@FindBy(xpath = "//div[@role='alert']")
+ public WebElement alertPopUp;
+
+
+
 
 }
